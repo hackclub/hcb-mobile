6d73f15cb25b7d14d93d685a76d433d55418f533
Caleb Denio <cjdenio44@gmail.com>
2023-09-12 14:25:40 -0400

    Update transaction icons

--- src/lib/types/Transaction.ts
+++ src/lib/types/Transaction.ts
@@ -1,23 +1,26 @@
+export enum TransactionType {
+  Unknown = "000",
+  Invoice = "100",
+  Donation = "200",
+  PartnerDonation = "201",
+  AchTransfer = "300",
+  Check = "400",
+  IncreaseCheck = "401",
+  CheckDeposit = "402",
+  Disbursement = "500",
+  StripeCard = "600",
+  StripeForceCapture = "601",
+  BankFee = "700",
+  IncomingBankFee = "701",
+  FeeRevenue = "702",
+  AchPayment = "800",
+}
+
 export default interface Transaction {
   id: string;
   date: string;
   amount_cents: number;
   memo: string;
   pending: boolean;
-  code:
-    | "000"
-    | "100"
-    | "200"
-    | "201"
-    | "300"
-    | "400"
-    | "401"
-    | "402"
-    | "500"
-    | "600"
-    | "601"
-    | "700"
-    | "701"
-    | "702"
-    | "800";
+  code: TransactionType;
 }
