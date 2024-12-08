151686abe2e0baf212f7a618fda43124b1bbb9de
Gaurav Pandey <hi@pandeygaurav.com>
2024-12-08 13:34:21 +0530

    we don't need this because we're using stripe

--- app.config.js
+++ app.config.js
@@ -29,9 +29,6 @@ export default {
         "applinks:hcb.hackclub.com",
         "applinks:bank.hackclub.com",
       ],
-      entitlements: {
-        "com.apple.developer.proximity-reader.payment.acceptance": true,
-      }
     },
     android: {
       icon: "./assets/app-icon.png",
