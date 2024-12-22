92618253684d0e183c3d57ce65dcd0e632a039e3
Mohamad Mortada <mmortada685@gmail.com>
2024-12-22 13:34:23 -0800

    Delete src/react-native-dynamic-app-icon.d.ts
    no longer needed changed libraries

--- src/react-native-dynamic-app-icon.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react-native-dynamic-app-icon" {
-  declare const AppIcon: {
-    setAppIcon: (name: string) => string;
-    getIconName: (callback: (result: { iconName: string }) => void) => void;
-  };
-
-  export default AppIcon;
-}
