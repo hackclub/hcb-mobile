073a7ee9f54c9508d03e7b9f258c12708a262d9a
Mohamad <mmortada685@gmail.com>
2024-12-21 23:51:49 -0800

    add reciepts created_at to modal

--- src/lib/types/Receipt.ts
+++ src/lib/types/Receipt.ts
@@ -3,6 +3,7 @@ import User from "./User";
 
 export default interface Receipt extends HcbApiObject<"rct"> {
   url: string;
+  created_at: string;
   preview_url?: string;
   filename: string;
   uploader?: User;
