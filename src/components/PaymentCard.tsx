d843290dbad38eea6245c1267457260e386f1200
Mohamad Mortada <mmortada685@gmail.com>
2024-12-27 17:26:26 -0800

    Temp fix for svg rendering

--- src/components/PaymentCard.tsx
+++ src/components/PaymentCard.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@react-navigation/native";
+import Constants from "expo-constants";
 import * as Geopattern from "geopattern";
 import { useEffect, useRef, useState } from "react";
 import {
@@ -118,7 +119,12 @@ export default function PaymentCard({
             height: (width * 0.86) / 1.5,
           }}
         >
-          <SvgUri uri={pattern} width={svgWidth} height={svgHeight} />
+          { Constants.platform?.android ? (
+            <SvgXml xml={patternForMeasurements} width={svgWidth} height={svgHeight} />
+          ) : (
+            <SvgUri uri={pattern} width={svgWidth} height={svgHeight} />
+          )
+          }
         </View>
       )}
 
