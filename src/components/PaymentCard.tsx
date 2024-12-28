2079c387d0315670822c047dbe7185ebc6d42d65
Mohamad Mortada <mmortada685@gmail.com>
2024-12-27 18:01:35 -0800

    Update PaymentCard.tsx

--- src/components/PaymentCard.tsx
+++ src/components/PaymentCard.tsx
@@ -10,7 +10,7 @@ import {
   AppState,
   useWindowDimensions,
 } from "react-native";
-import { SvgUri } from "react-native-svg";
+import { SvgUri, SvgXml } from "react-native-svg";
 
 import Card from "../lib/types/Card";
 import { CardDetails } from "../lib/useStripeCardDetails";
