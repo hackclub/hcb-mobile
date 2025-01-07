8168c579c4bbdc4af87e27f8a2290231b2426da7
Mohamad Mortada <mmortada685@gmail.com>
2025-01-07 11:27:01 -0800

    Playground Banner Design Changes (#107)

--- src/components/organizations/PlaygroundBanner.tsx
+++ src/components/organizations/PlaygroundBanner.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 import Organization from "../../lib/types/Organization";
 import { palette } from "../../theme";
@@ -12,6 +12,9 @@ export default function PlaygroundBanner({
     <View
       style={{
         backgroundColor: "rgb(43, 65, 95)",
+        borderColor: "rgb(52, 152, 219)", 
+        borderWidth: 2,
+        borderStyle: "dotted",
         padding: 10,
         marginBottom: 20,
         borderRadius: 8,
@@ -19,18 +22,20 @@ export default function PlaygroundBanner({
     >
       <View
         style={{
-          flexDirection: "row",
           alignItems: "center",
-          justifyContent: "space-between",
+          justifyContent: "center",
         }}
       >
-        <Text style={{ color: palette.smoke, flex: 1 }}>
-          To raise & spend money, activate your organization&apos;s account.
+        <Text style={{ color: palette.smoke, fontWeight: "bold", marginBottom: 5 }}>
+          Playground Mode
         </Text>
-        <Button
+        <Text style={{ color: palette.smoke, flex: 1, textAlign: "center" }}>
+          To raise & spend money, wait for your organization&apos;s account to be activated by a staff member.
+        </Text>
+        {/* <Button
           title="Activate"
           disabled={organization.playground_mode_meeting_requested}
-        />
+        /> */}
       </View>
     </View>
   );
