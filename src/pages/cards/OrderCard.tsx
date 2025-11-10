92ff4144e56cb73f5978fd1d76abb2e23ebb1602
Mohamad Mortada <mmortada685@gmail.com>
2025-11-09 22:49:10 -0800

    Update cardDesigns fetch URL based on organizationId

--- src/pages/cards/OrderCard.tsx
+++ src/pages/cards/OrderCard.tsx
@@ -60,8 +60,8 @@ export default function OrderCardScreen({ navigation }: Props) {
   const [expandedOrganizations, setExpandedOrganizations] = useState<
     Record<string, OrganizationExpanded>
   >({});
-  const { data: cardDesigns } = useOfflineSWR<CardDesign[]>(
-    `cards/card_designs?event_id=${organizationId}`,
+  const { data: cardDesigns } = useOfflineSWR<CardDesign[]>(organizationId ?
+    `cards/card_designs?event_id=${organizationId}` : `cards/card_designs`,
   );
   const hcb = useClient();
   const { data: organizations } = useSWR<Organization[]>("user/organizations");
