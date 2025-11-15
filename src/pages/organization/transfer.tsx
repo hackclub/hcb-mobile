f92d446d34d01212a018fa4451a52168a3fb948a
Mohamad Mortada <mmortada685@gmail.com>
2025-11-14 19:52:56 -0800

    lint

--- src/pages/organization/transfer.tsx
+++ src/pages/organization/transfer.tsx
@@ -1,6 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Constants from "expo-constants";
 import { useEffect, useState } from "react";
 import {
   KeyboardAvoidingView,
