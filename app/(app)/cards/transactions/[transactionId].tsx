a7965d6bd9fe7ec84e3cfcb00dbf6387c1285759
Manu G <manusvathgurudath@gmail.com>
2026-03-06 01:16:26 -0800

    Format JSON and expand TSX type annotation

--- app/(app)/cards/transactions/[transactionId].tsx
+++ app/(app)/cards/transactions/[transactionId].tsx
@@ -2,6 +2,10 @@ import TransactionPage from "app/(app)/(events)/[id]/transactions/[transactionId
 import { useLocalSearchParams } from "expo-router";
 
 export default function Page() {
-  const params = useLocalSearchParams<{ transactionId: string; orgId?: string; transaction?: string }>();
+  const params = useLocalSearchParams<{
+    transactionId: string;
+    orgId?: string;
+    transaction?: string;
+  }>();
   return <TransactionPage data={params} />;
 }
